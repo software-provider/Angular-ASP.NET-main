b9e966c63b3f94e67d8baf46e5f3d9ac0ef883e2
Software Provider <118369443+software-provider@users.noreply.github.com>
2023-06-21 07:08:04 +0900

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
+      require('karma-jasmine'), 
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
